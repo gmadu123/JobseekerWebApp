248ead10d5d288a1dbee2720572552cfb406bdd2
gopimudhapati <gmudhapati@hotmail.com>
2015-11-21 17:38:33 -0600

    test

--- JobSeekerWebApp/src/com/g2force/jobseeker/web/servlets/RegServlet.java
+++ JobSeekerWebApp/src/com/g2force/jobseeker/web/servlets/RegServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class RegServlet
  */
-@WebServlet("/HTML/RegServlet")
+@WebServlet("/RegServlet")
 public class RegServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
